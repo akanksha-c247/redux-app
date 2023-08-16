ce74f02a296214937d4848abd5f3e7c8ef5b4fee
prabha-c247 <141140574+prabha-c247@users.noreply.github.com>
2023-08-16 13:43:45 +0530

    rdux counter

---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+// Counter.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../redux/Action';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <Typography variant="h4">Counter: {counter}</Typography>
+      <Button variant="contained" color="primary" onClick={() => dispatch(increment())}>
+        Increment
+      </Button>
+      <Button variant="contained" color="secondary" onClick={() => dispatch(decrement())}>
+        Decrement
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
