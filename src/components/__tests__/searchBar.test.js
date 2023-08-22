db02ec4440fc02ba38810479f8c2345863c55121
akanksha-c247 <akanksha.k@chapter247.com>
2023-08-22 19:23:53 +0530

    jest testing

--- src/components/__tests__/searchBar.test.js
+++ src/components/__tests__/searchBar.test.js
@@ -28,10 +28,8 @@ describe('SearchAppBar', () => {
     render(<SearchAppBar />);
     const searchInput = screen.getByPlaceholderText('Search…');
 
-    // Simulate user entering text in the search input
     userEvent.type(searchInput, 'search term');
 
-    // Assert that the dispatch function was called with the correct action
     expect(mockDispatch).toHaveBeenCalledWith(filterUsers('search term'));
   });
 
@@ -39,16 +37,12 @@ describe('SearchAppBar', () => {
     render(<SearchAppBar />);
     const searchInput = screen.getByPlaceholderText('Search…');
 
-    // Simulate user entering text in the search input
     userEvent.type(searchInput, 'search term');
 
-    // Get the clear button
     const clearButton = screen.getByRole('button', { name: /clear/i });
 
-    // Simulate user clicking the clear button
     fireEvent.click(clearButton);
 
-    // Wait for the input value to be cleared
     await waitFor(() => expect(searchInput).toHaveValue(''));
   });
 });
